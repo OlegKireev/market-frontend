62fe4d14ab06cc6ba61d46aa345974e764b536b6
Oleg Kireev <okireev@ubic.tech>
2022-06-07 09:19:51 +0300

    pages/catalog: create

---
+++ pages/catalog.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const CatalogPage: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Catalog</title>
+        <meta name="description" content="Catalog" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Catalog
+        </h1>
+
+      </main>
+    </div>
+  )
+}
+
+export default CatalogPage
