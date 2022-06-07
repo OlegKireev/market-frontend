e2630aa12ad995d9956ed41cd2a854fc2f5228af
Oleg Kireev <okireev@ubic.tech>
2022-06-07 09:51:11 +0300

    components/Container: create

---
+++ components/Container/indet.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+interface IContainer {
+  children: React.ReactNode;
+}
+
+const Container = ({ children }: IContainer) => {
+  return (
+    <div className={styles.container}>
+      { children }
+    </div>
+  )
+}
+
+export default Container;
